3dca13dd97415031e0a4befd868072fba77e56b8
John Michael Singayan <jsingayan54@gmail.com>
2017-12-05 16:11:45 +0800

    modify code on getting places
    get only the first 2 array from the address in get places. first array for the search display place

--- search_display_selected_places.php
+++ search_display_selected_places.php
@@ -64,63 +64,66 @@
 				$explode_city[$i] = str_replace(" City", "", $explode_city[$i]);
 			}
 			
-			$search['select'] = "location_id, v.user_id, v.place_id, e.place_name, v.location as address, v.coordinates, firstname, lastname, username, profile_pic, video_id, video_name, description, v.video_file, video_thumb, date_upload, date_expiry, view_count, like_count, video_length, landscape_file";
-			$search['table'] = "veeds_establishment e, veeds_users u, veeds_videos v";
-			$search['where'] = "v.place_id = e.place_id 
-								AND v.user_id = u.user_id
-								AND (v.location LIKE '%".$explode_city[$i]."%'
-								OR e.place_name LIKE '%".$explode_city[$i]."%')".$u_extend_names;
-			$search['filters'] = "GROUP BY location_id ORDER BY date_upload DESC";
-			// echo implode(" ", $search);
-			if(jp_count($search) > 0){
+			if($i < 1){
 
-				$result = jp_get($search);
-				while ($row = mysqli_fetch_assoc($result)) {
+				$search['select'] = "location_id, v.user_id, v.place_id, e.place_name, v.location as address, v.coordinates, firstname, lastname, username, profile_pic, video_id, video_name, description, v.video_file, video_thumb, date_upload, date_expiry, view_count, like_count, video_length, landscape_file";
+				$search['table'] = "veeds_establishment e, veeds_users u, veeds_videos v";
+				$search['where'] = "v.place_id = e.place_id 
+									AND v.user_id = u.user_id
+									AND (v.location LIKE '%".$explode_city[$i]."%'
+									OR e.place_name LIKE '%".$explode_city[$i]."%')".$u_extend_names;
+				$search['filters'] = "GROUP BY location_id ORDER BY date_upload DESC";
+				// echo implode(" ", $search);
+				if(jp_count($search) > 0){
 
-					$search1['select'] = "like_type";
-					$search1['table'] = "veeds_videos_likes";
-					$search1['where'] = "video_id = '".$row['video_id']."' 
-											AND user_id = '".$row['user_id']."'
-											AND user_id_liker = '".$_POST['user_id']."'";
+					$result = jp_get($search);
+					while ($row = mysqli_fetch_assoc($result)) {
 
-					// echo implode(" ", $search1)."<br>";
-					if(jp_count($search1) > 0){
+						$search1['select'] = "like_type";
+						$search1['table'] = "veeds_videos_likes";
+						$search1['where'] = "video_id = '".$row['video_id']."' 
+												AND user_id = '".$row['user_id']."'
+												AND user_id_liker = '".$_POST['user_id']."'";
 
-						$result1 = jp_get($search1);
-						while ($row1 = mysqli_fetch_assoc($result1)) {
-							
-							$row['like_type'] = $row1['like_type'];
+						// echo implode(" ", $search1)."<br>";
+						if(jp_count($search1) > 0){
+
+							$result1 = jp_get($search1);
+							while ($row1 = mysqli_fetch_assoc($result1)) {
+								
+								$row['like_type'] = $row1['like_type'];
+							}
+						}else{
+							$row['like_type'] = "";
 						}
-					}else{
-						$row['like_type'] = "";
-					}
 
-					$row = array(
-									'category' => "Places",
-									'location_id' => $row['location_id'],
-									'place_id' => $row['place_id'],
-									'place_name' => $row['place_name'],
-									'user_id' => $row['user_id'],
-									'firstname' => $row['firstname'],
-									'lastname' => $row['lastname'],
-									'username' => $row['username'],
-									'profile_pic' => $row['profile_pic'],
-									'video_id' => $row['video_id'],
-									'video_name' => $row['video_name'],
-									'description' => $row['description'],
-									'video_file' => $row['video_file'],
-									'video_thumb' => $row['video_thumb'],
-									'date_upload' => $row['date_upload'],
-									'date_expiry' => $row['date_expiry'],
-									'view_count' => (int)$row['view_count'],
-									'like_count' => (int)$row['like_count'],
-									'address' => $row['address'],
-									'video_length' => (int)$row['video_length'],
-									'landscape_file' => $row['landscape_file'],
-									'like_type' => $row['like_type'],
-									'logged_id' => $_POST['user_id']
-								);
-					$list[] = $row;	
+						$row = array(
+										'category' => "Places",
+										'location_id' => $row['location_id'],
+										'place_id' => $row['place_id'],
+										'place_name' => $row['place_name'],
+										'user_id' => $row['user_id'],
+										'firstname' => $row['firstname'],
+										'lastname' => $row['lastname'],
+										'username' => $row['username'],
+										'profile_pic' => $row['profile_pic'],
+										'video_id' => $row['video_id'],
+										'video_name' => $row['video_name'],
+										'description' => $row['description'],
+										'video_file' => $row['video_file'],
+										'video_thumb' => $row['video_thumb'],
+										'date_upload' => $row['date_upload'],
+										'date_expiry' => $row['date_expiry'],
+										'view_count' => (int)$row['view_count'],
+										'like_count' => (int)$row['like_count'],
+										'address' => $row['address'],
+										'video_length' => (int)$row['video_length'],
+										'landscape_file' => $row['landscape_file'],
+										'like_type' => $row['like_type'],
+										'logged_id' => $_POST['user_id']
+									);
+						$list[] = $row;	
+					}
 				}
 			}	
 		}
--- search_display_selected_places_dup.php
+++ search_display_selected_places_dup.php
@@ -10,7 +10,7 @@
 	}
 
 	$_POST['user_id'] = "271";
-	$_POST['keyword'] = "Dona Imelda, Quezon City, Metro Manila, Philippines";
+	$_POST['keyword'] = "Mandaluyong, Metro Manila, Philippines";
 	if(isset($_POST['user_id']) && isset($_POST['keyword'])){
 
 		// $placeIdDetail = new classPlaceID;
@@ -22,6 +22,7 @@
 		$block['table'] = "veeds_users_block";
 		$block['where'] = "user_id = ".$_POST['user_id'];
 		$result_block = jp_get($block);
+		
 		while($row3 = mysqli_fetch_assoc($result_block)){
 			$u_blocks[] = $row3['user_id_block'];
 		}
@@ -248,75 +249,77 @@
 		$explode_city = explode(",", $_POST['keyword']);
 		for($i = 0; $i < count($explode_city); $i++){
 			
-			// if(strpos($explode_city[$i], 'City') !== FALSE){
-			// 	$explode_city[$i] = str_replace(" City", "", $explode_city[$i]);
-			// }
-			// echo $explode_city[$i]."<br>";
+			if(strpos($explode_city[$i], 'City') !== FALSE){
+				$explode_city[$i] = str_replace(" City", "", $explode_city[$i]);
+			}
+			echo $explode_city[$i]."<br>";
+			if($i < 1){
 
-			$search['select'] = "location_id, v.user_id, v.place_id, e.place_name, v.location as address, v.coordinates, firstname, lastname, username, profile_pic, video_id, video_name, description, v.video_file, video_thumb, date_upload, date_expiry, view_count, like_count, video_length, landscape_file";
-			$search['table'] = "veeds_establishment e, veeds_users u, veeds_videos v";
-			$search['where'] = "v.place_id = e.place_id 
-								AND v.user_id = u.user_id
-								AND (v.location LIKE '%".$explode_city[$i]."%'
-								OR e.place_name LIKE '%".$explode_city[$i]."%')".$u_extend_names;
-			$search['filters'] = "GROUP BY location_id ORDER BY date_upload DESC";
-			// echo implode(" ", $search);
-			if(jp_count($search) > 0){
+				$search['select'] = "location_id, v.user_id, v.place_id, e.place_name, v.location as address, v.coordinates, firstname, lastname, username, profile_pic, video_id, video_name, description, v.video_file, video_thumb, date_upload, date_expiry, view_count, like_count, video_length, landscape_file";
+				$search['table'] = "veeds_establishment e, veeds_users u, veeds_videos v";
+				$search['where'] = "v.place_id = e.place_id 
+									AND v.user_id = u.user_id
+									AND (v.location LIKE '%".$explode_city[$i]."%'
+									OR e.place_name LIKE '%".$explode_city[$i]."%')".$u_extend_names;
+				$search['filters'] = "GROUP BY location_id ORDER BY date_upload DESC";
+				// echo implode(" ", $search);
+				if(jp_count($search) > 0){
 
-				$result = jp_get($search);
-				while ($row = mysqli_fetch_assoc($result)) {
+					$result = jp_get($search);
+					while ($row = mysqli_fetch_assoc($result)) {
 
-					$search1['select'] = "like_type";
-					$search1['table'] = "veeds_videos_likes";
-					$search1['where'] = "video_id = '".$row['video_id']."' 
-											AND user_id = '".$row['user_id']."'
-											AND user_id_liker = '".$_POST['user_id']."'";
+						$search1['select'] = "like_type";
+						$search1['table'] = "veeds_videos_likes";
+						$search1['where'] = "video_id = '".$row['video_id']."' 
+												AND user_id = '".$row['user_id']."'
+												AND user_id_liker = '".$_POST['user_id']."'";
 
-					// echo implode(" ", $search1)."<br>";
-					if(jp_count($search1) > 0){
+						// echo implode(" ", $search1)."<br>";
+						if(jp_count($search1) > 0){
 
-						$result1 = jp_get($search1);
-						while ($row1 = mysqli_fetch_assoc($result1)) {
-							
-							$row['like_type'] = $row1['like_type'];
+							$result1 = jp_get($search1);
+							while ($row1 = mysqli_fetch_assoc($result1)) {
+								
+								$row['like_type'] = $row1['like_type'];
+							}
+						}else{
+							$row['like_type'] = "";
 						}
-					}else{
-						$row['like_type'] = "";
-					}
-					
-					// sleep(2);
-					// $placeIdDetail->setPlaceId($row['place_id']);
-					// $placeName = $placeIdDetail->getPlaceName();
+						
+						// sleep(2);
+						// $placeIdDetail->setPlaceId($row['place_id']);
+						// $placeName = $placeIdDetail->getPlaceName();
 
-					$row = array(
-									'category' => "Places",
-									'location_id' => $row['location_id'],
-									'place_id' => $row['place_id'],
-									// 'place_name' => $placeName,
-									'place_name' => $row['place_name'],
-									'user_id' => $row['user_id'],
-									'firstname' => $row['firstname'],
-									'lastname' => $row['lastname'],
-									'username' => $row['username'],
-									'profile_pic' => $row['profile_pic'],
-									'video_id' => $row['video_id'],
-									'video_name' => $row['video_name'],
-									'description' => $row['description'],
-									'video_file' => $row['video_file'],
-									'video_thumb' => $row['video_thumb'],
-									'date_upload' => $row['date_upload'],
-									'date_expiry' => $row['date_expiry'],
-									'view_count' => (int)$row['view_count'],
-									'like_count' => (int)$row['like_count'],
-									'address' => $row['address'],
-									'video_length' => (int)$row['video_length'],
-									'landscape_file' => $row['landscape_file'],
-									'like_type' => $row['like_type'],
-									'logged_id' => $_POST['user_id']
-								);
-					$list[] = $row;	
-				}
-			}	
+						$row = array(
+										'category' => "Places",
+										'location_id' => $row['location_id'],
+										'place_id' => $row['place_id'],
+										// 'place_name' => $placeName,
+										'place_name' => $row['place_name'],
+										'user_id' => $row['user_id'],
+										'firstname' => $row['firstname'],
+										'lastname' => $row['lastname'],
+										'username' => $row['username'],
+										'profile_pic' => $row['profile_pic'],
+										'video_id' => $row['video_id'],
+										'video_name' => $row['video_name'],
+										'description' => $row['description'],
+										'video_file' => $row['video_file'],
+										'video_thumb' => $row['video_thumb'],
+										'date_upload' => $row['date_upload'],
+										'date_expiry' => $row['date_expiry'],
+										'view_count' => (int)$row['view_count'],
+										'like_count' => (int)$row['like_count'],
+										'address' => $row['address'],
+										'video_length' => (int)$row['video_length'],
+										'landscape_file' => $row['landscape_file'],
+										'like_type' => $row['like_type'],
+										'logged_id' => $_POST['user_id']
+									);
+						$list[] = $row;	
+					}
+				}	
+			}
 		}
 		
 		echo json_encode($list);
